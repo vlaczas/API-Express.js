d397365f91dec09b942e552859a533734e1a1798
Wladislaw Czasowski <czasowskij@gmail.com>
2021-03-05 00:12:21 +0200

    CRUD operations implemented

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// LOAD env vars
+const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+/**
+ * *Load env
+ */
 dotenv.config({ path: './config/config.env' });
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//Body parser
+app.use(express.json());
 
-app.listen(PORT, () => {
+// Connect to DB
+connectDB();
+
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+/**
+ * *MOUNT routers
+ */
+
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
   console.log(
-    'Server running in ' + process.env.NODE_ENV + ' mode pn port ' + PORT
+    `Server running in ${process.env.NODE_ENV} mode pn port ${PORT}`.yellow.bold
   );
+});
+
+/**
+ * * Handle unhandled promise rejectrions
+ */
+process.on('unhandledRejection', (err, promise) => {
+  console.error(`Error: ${err.message}`.red);
+  //close server and exit proccess
+  server.close(() => {
+    process.exit(1);
+  });
 });
